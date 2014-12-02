f616cf3bab786b1849628e05d1598a001c6219a4
Abimael Martell <abimex@gmail.com>
2014-12-01 23:33:47 -0800

    add uglify for vendors minifying

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,8 +12,23 @@ module.exports = function(grunt) {
                     "assets/css/main.css": "assets/less/main.less"
                 }
             }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd h:M o") %> */\n'
+            },
+            vendor: {
+                files: {
+                    'assets/js/vendor.min.js': [
+                        'assets/vendor/jquery/dist/jquery.js'
+                    ]
+                }
+            }
         }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
